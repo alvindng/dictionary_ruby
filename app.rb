1f1817c83c223b4987f354d7e805ba24cb211a22
Alvin Ng <alvindng@gmail.com>
2016-07-08 14:47:55 -0700

    adding to app.rb for proper routing of user inputs and adding the routes into the erb pages

--- app.rb
+++ app.rb
@@ -2,9 +2,14 @@ require('sinatra')
 require('sinatra/reloader')
 also_reload('lib/**/*.rb')
 require('./lib/words')
+require('./lib/definitions')
 
 get('/') do
   erb(:index)
+end
+
+get('/words/new') do
+  erb(:words_form)
 end
 
 get('/words') do
@@ -12,18 +17,30 @@ get('/words') do
   erb(:words)
 end
 
-get('words/new') do
-  erb(:words_form)
-end
-
 post('/words') do
   word = params.fetch('word')
-  new_word = Word.new(word)
-  new_word.save()
-  erb(:success)
+  Word.new(word).save()
+  @words = Word.all()
+  erb(:words)
 end
 
 get('/words/:id') do
-  @word = Word.find(params.fetch("id"))
+  @word = Word.find(params.fetch("id").to_i())
   erb(:word)
+end
+
+post('/words/:id') do
+  @word = Word.find(params.fetch("id").to_i())
+  definition = params.fetch('definition')
+  @definition = Definition.new(definition)
+  @definition.save()
+  @definitions = Definition.all()
+  @word = Word.find(params.fetch('word_id').to_i())
+  @word.add_definition(@definition)
+  erb(:word)
+end
+
+get('/words/:id/definitions/new') do
+  @word = Word.find(params.fetch('id').to_i())
+  erb(:definitions_form)
 end
