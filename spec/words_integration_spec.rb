0b4c69da5bf3cbf78c0939012ba2d52528742a5a
Alvin Ng <alvindng@gmail.com>
2016-07-08 15:31:56 -0700

    add spec for landing page visit

--- spec/words_integration_spec.rb
+++ spec/words_integration_spec.rb
@@ -1,2 +1,12 @@
 require('capybara/rspec')
 require('./app')
+require('launchy')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('the user path', {:type => :feature}) do
+  it('opens the home page') do
+    visit('/')
+    expect(page).to have_content("Welcome to Your Dictionary!")
+  end
+end
