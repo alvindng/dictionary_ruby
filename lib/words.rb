01945c7d59ca55afe85ce58459fd4acb84139959
Alvin Ng <alvindng@gmail.com>
2016-07-08 09:41:04 -0700

    add method and spec to find word based on its id

--- lib/words.rb
+++ lib/words.rb
@@ -25,4 +25,15 @@ class Word
   define_method(:id) do
     @id
   end
+
+  define_singleton_method(:find) do |identification|
+    found_word = nil
+    @@words.each() do |word|
+      if word.id().eql?(identification.to_i())
+        found_word = word
+      end
+    end
+    found_word
+  end
+
 end
