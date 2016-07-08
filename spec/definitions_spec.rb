e6c4445c4df40fd1520d02d2e39cf4e603b2a056
Alvin Ng <alvindng@gmail.com>
2016-07-08 11:15:13 -0700

    adds a spec and method for clearing out the array before testing rspec

--- spec/definitions_spec.rb
+++ spec/definitions_spec.rb
@@ -2,8 +2,11 @@ require('rspec')
 require('definitions')
 
 describe('Definition') do
+  before() do
+  Definition.clear()  
+  end
 
-  describe(':definition') do
+  describe('#definition') do
     it "returns a definition entered by a user" do
       test_definition = Definition.new("a greeting")
       expect(test_definition.definition()).to(eq("a greeting"))
@@ -17,11 +20,18 @@ describe('Definition') do
     end
   end
 
-  describe(':save') do
+  describe('#save') do
     it('saves the new definition into the array of definitions') do
       test_definition = Definition.new('a greeting')
       test_definition.save()
       expect(Definition.all()).to(eq([test_definition]))
+    end
+  end
+
+  describe('.clear') do
+    it('empties out all of the saved definitions') do
+      Definition.clear()
+      expect(Definition.all()).to(eq([]))
     end
   end
 end
