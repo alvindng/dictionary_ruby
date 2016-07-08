14f044722e75501beb114c7889d8697c7dbcfb95
Alvin Ng <alvindng@gmail.com>
2016-07-08 10:35:15 -0700

    begin assigning routes in the app.rb

--- app.rb
+++ app.rb
@@ -6,3 +6,24 @@ require('./lib/words')
 get('/') do
   erb(:index)
 end
+
+get('/words') do
+  @words = Word.all()
+  erb(:words)
+end
+
+get('words/new') do
+  erb(:words_form)
+end
+
+post('/words') do
+  word = params.fetch('word')
+  new_word = Word.new(word)
+  new_word.save()
+  erb(:success)
+end
+
+get('/words/:id') do
+  @word = Word.find(params.fetch("id"))
+  erb(:word)
+end
